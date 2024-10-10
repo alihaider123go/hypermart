c782ce96b4e8b97d055a1308dd2043b9fa040bd9
Ali Haider <alihaider123go@gmail.com>
2024-10-10 12:27:27 +0500

    Add Whatsapp FAB

---
+++ lib/util/app_constants.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+
+  // User Open WhatsApp
+  static const String whatsAppIOSUri = 'https://wa.me/+2341234567890';
+  static const String whatsAppAndroidUri = 'whatsapp://send?phone=+2341234567890';
+  static const String whatsAppWebUri = 'https://web.whatsapp.com/send?phone=+2341234567890';
+
+}
